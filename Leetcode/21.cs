cf05a494c47aa2a3d80c00e5d86cd782d89131ca
sm.mostafajamal@gmail.com <sm.mostafajamal@gmail.com>
2026-06-04 22:42:13 +0600

    https://leetcode.com/problems/merge-two-sorted-lists

---
+++ Leetcode/21.cs
@@ -0,0 +1,47 @@
+ public class ListNode {
+     public int val;
+     public ListNode next;
+ 
+     public ListNode(int val=0, ListNode next=null) {
+         this.val = val;
+         this.next = next;
+     }
+ }
+
+public class Solution {
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode head = new ListNode();
+        var current = head;
+
+        while(list1 != null && list2 != null) 
+        {
+            if(list1.val <= list2.val)
+            {
+                current.next = list1;
+                list1 = list1.next;
+            }
+            else
+            {
+                current.next = list2;
+                list2 = list2.next;
+            }
+
+            current = current.next;
+        }
+
+        if(list1 is not null)
+        {
+            current.next = list1;
+        }
+        else
+        {
+            current.next = list2;
+        }
+
+        return head.next;
+    }
+}
+
+// var sol = new Solution();
+
+// sol.MergeTwoLists(new ListNode(1, new ListNode(2, new ListNode(4))), new ListNode(1, new ListNode(3, new ListNode(4))));
